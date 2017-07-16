55277c37ca9c8560e474cfbae997329bd9c6573d
Sandeepa1995 <damithalenadora@yahoo.com.au>
2017-07-17 01:14:42 +0530

    Integrated Event creation

--- view/js/create-event.js
+++ view/js/create-event.js
@@ -273,16 +273,18 @@ function getSelectionList() {
         if(err){
             return console.log(err);
         }
-        for (var indx = 0; indx < listItems.department.length; ++indx) {
-            var deptListItem = '<option>'+listItems.department[indx]+'</option>';
-            $("#crtRegularDepartment").append(deptListItem);
-            //console.log(listItems.departments[indx]);
-        }
+        if(listItems!=undefined) {
+            for (var indx = 0; indx < listItems.department.length; ++indx) {
+                var deptListItem = '<option>' + listItems.department[indx] + '</option>';
+                $("#crtRegularDepartment").append(deptListItem);
+                //console.log(listItems.departments[indx]);
+            }
 
-        for (var indx2 = 0; indx2 < listItems.year.length; ++indx2) {
-            var batchListItem = '<option>'+listItems.year[indx2]+'</option>';
-            $("#crtRegularBatch").append(batchListItem);
-            //console.log(listItems.years[indx2]);
+            for (var indx2 = 0; indx2 < listItems.year.length; ++indx2) {
+                var batchListItem = '<option>' + listItems.year[indx2] + '</option>';
+                $("#crtRegularBatch").append(batchListItem);
+                //console.log(listItems.years[indx2]);
+            }
         }
     });
 }
